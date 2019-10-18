2b273451c45629f7748d86529994aa900a129952
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-18 08:18:21 +0100

    :sparkles: Build Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+  const initialForm = {
+    username: '',
+    password: ''
+  }
+  return <Formik
+    initialValues = {initialForm}
+    onSubmit = {() => {}}
+    render = {props => <Form>
+        <h3>Login</h3>
+        <Field name='username' type='text' placeholder='Username' />
+        <Field name='password' type='password' placeholder='Password' />
+      </Form>
+    }
+  />;
 };
 
 export default Login;
