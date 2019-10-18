b541c72332b9342d172f01e93b1c0decd577edd1
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-18 08:36:17 +0100

    :sparkles: Create nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,7 +9,12 @@ function App() {
   return (
     <Router>
       <div className="App">
+      <nav>
+          <NavLink to='/'>Login</NavLink>
+          <NavLink to='/bubbles'>See Bubbles</NavLink>
+        </nav>
         <Route exact path="/" component={Login} />
+        
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
