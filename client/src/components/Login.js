cd188762b37477e765cd99d6cfea37a992d017cd
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-18 08:33:39 +0100

    :sparkles: Complete login feature
    ...redirect to /bubbles on successful login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
 import { Button } from "react-bulma-components";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = ({ history }) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const initialForm = {
     username: '',
     password: ''
   }
+
+  const attemptLogin = (formValues, actions) => {
+    axiosWithAuth().post('/login', formValues)
+    .then(({ data }) => {
+      localStorage.setItem('token', data.payload)
+      actions.resetForm();
+      history.push('/bubbles')
+    })
+    .catch(err => alert(err))
+  }
   return <Formik
     initialValues = {initialForm}
-    onSubmit = {() => {}}
+    onSubmit = {attemptLogin}
     render = {props => <Form>
         <h3>Login</h3>
         <Field name='username' type='text' placeholder='Username' />
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosWithAuth = () => {
     const instance = axios.create({
-        baseURL: 'http://localhost:5000',
+        baseURL: 'http://localhost:5000/api',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': localStorage.getItem('token'),
